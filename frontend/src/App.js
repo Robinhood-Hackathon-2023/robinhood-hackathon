05596ba2293a550dcbc7cc3b321f0c149bce8a5c
Tariqkharsa <Tariq.kharsa@gmail.com>
2023-09-29 23:09:51 -0500

    Add scss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import ForumPost from './Components/ForumPost'; // Assuming the component is in a file called RedditPost.js
+import React from "react";
+import ForumPost from "./Components/ForumPost"; // Assuming the component is in a file called RedditPost.js
 
 const posts = [
   {
     id: 1,
-    title: 'React Component Example',
-    content: 'This is a simple Reddit-style post component in React.',
+    title: "Mental Health Issue",
+    content: "I have been suffering from Depression .......",
     votes: 10,
-    comments: [
+    /*comments: [
       { id: 1, user: 'user1', text: 'Great post!' },
       { id: 2, user: 'user2', text: 'I learned a lot from this.' },
-    ],
+    ],*/
   },
   {
     id: 1,
-    title: 'React Component Example',
-    content: 'This is a simple Reddit-style post component in React.',
+    title: "Stress",
+    content: "I am having so much stress at work ...........",
     votes: 10,
-    comments: [
+    /*comments: [
       { id: 1, user: 'user1', text: 'Great post!' },
       { id: 2, user: 'user2', text: 'I learned a lot from this.' },
-    ],
-  }
+    ],*/
+  },
   // Add more posts as needed
 ];
 
 const App = () => {
   return (
     <div>
-      <h1>Reddit-style Posts</h1>
+      <h1 className="App-header">Posts</h1>
       {posts.map((post) => (
         <ForumPost key={post.id} post={post} />
       ))}
--- frontend/src/Components/ForumPost.jsx
+++ frontend/src/Components/ForumPost.jsx
@@ -1,16 +1,27 @@
-import React, { useState } from 'react';
-import { Card, CardContent, Typography, IconButton, Badge, TextField, Button } from '@material-ui/core';
-import { ThumbUp, ThumbDown, Comment } from '@material-ui/icons';
-import './css/ForumPost.css';
+import React, { useState } from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  IconButton,
+  Badge,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import { ArrowUpward, ArrowDownward, Comment } from "@material-ui/icons";
+import "./scss/ForumPost.scss";
 
 const CommentSection = ({ comments }) => {
   return (
     <div>
-      {comments.map((comment) => (
-        <div key={comment.id}>
-          <Typography>{comment.user}: {comment.text}</Typography>
-        </div>
-      ))}
+      {comments &&
+        comments.map((comment) => (
+          <div key={comment.id}>
+            <Typography>
+              {comment.user}: {comment.text}
+            </Typography>
+          </div>
+        ))}
     </div>
   );
 };
@@ -18,13 +29,13 @@ const CommentSection = ({ comments }) => {
 const ForumPost = ({ post }) => {
   const [votes, setVotes] = useState(post.votes);
   const [showCommentInput, setShowCommentInput] = useState(false);
-  const [newComment, setNewComment] = useState('');
+  const [newComment, setNewComment] = useState("");
 
   const handleVote = (type) => {
     // Simulate updating votes (you would typically send this to a server)
-    if (type === 'upvote') {
+    if (type === "upvote") {
       setVotes(votes + 1);
-    } else if (type === 'downvote') {
+    } else if (type === "downvote") {
       setVotes(votes - 1);
     }
   };
@@ -39,62 +50,71 @@ const ForumPost = ({ post }) => {
 
   const handleAddComment = () => {
     // Simulate adding a new comment (you would typically send this to a server)
-    console.log('New Comment:', newComment);
+    console.log("New Comment:", newComment);
 
     // Clear the comment input and hide it
-    setNewComment('');
+    setNewComment("");
     setShowCommentInput(false);
   };
 
   return (
     <Card className="cardContainer">
-    <CardContent className="cardContent">
-      <Typography variant="h6" className="title">{post.title}</Typography>
-      <Typography className="content">{post.content}</Typography>
+      <CardContent className="cardContent">
+        <Typography variant="h6" className="title">
+          {post.title}
+        </Typography>
+        <Typography className="content">{post.content}</Typography>
 
-      <div className="votingSection">
-          <IconButton color="primary" onClick={() => handleVote('upvote')}>
-            <ThumbUp />
+        <div className="votingSection">
+          <IconButton color="primary" onClick={() => handleVote("upvote")}>
+            <ArrowUpward style={{ color: "green" }} />
+          </IconButton>
+          <IconButton color="primary" onClick={() => handleVote("downvote")}>
+            <ArrowDownward style={{ color: "red" }} />
           </IconButton>
           <Badge badgeContent={votes} color="primary">
-            <span style={{ margin: '0 10px' }}>Votes</span>
+            <span style={{ margin: "0 10px" }}>Votes</span>
           </Badge>
-          <IconButton color="primary" onClick={() => handleVote('downvote')}>
-            <ThumbDown />
-          </IconButton>
-      </div>
-
-      <div className="commentsSection">
-          <IconButton color="primary" onClick={handleToggleCommentInput}>
-            <Comment />
-          </IconButton>
-          <span style={{ marginLeft: '5px' }}>{post.comments.length} comments</span>
-      </div>
-
-      <CommentSection comments={post.comments} />
-
-      {showCommentInput && (
-        <div className="commentInputSection">
-          <TextField
-            label="Your Comment"
-            variant="outlined"
-            multiline
-            rows={2}
-            fullWidth
-            className="commentInput"
-            value={newComment}
-            onChange={handleCommentChange}
-          />
-          <Button variant="contained" color="primary" onClick={handleAddComment} className="addCommentButton">
-            Add Comment
-          </Button>
         </div>
-      )}
-    </CardContent>
-  </Card>
+
+        <div className="commentsSection">
+          <IconButton color="primary" onClick={handleToggleCommentInput}>
+            <Comment style={{ color: "#9b9898" }} />
+          </IconButton>
+          {post.comments && (
+            <span style={{ marginLeft: "5px" }}>
+              {post.comments.length} comments
+            </span>
+          )}
+        </div>
+
+        {post.comments && <CommentSection comments={post.comments} />}
+
+        {showCommentInput && (
+          <div className="commentInputSection">
+            <TextField
+              label="Your Comment"
+              variant="outlined"
+              multiline
+              rows={2}
+              fullWidth
+              className="commentInput"
+              value={newComment}
+              onChange={handleCommentChange}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleAddComment}
+              className="addCommentButton"
+            >
+              Add Comment
+            </Button>
+          </div>
+        )}
+      </CardContent>
+    </Card>
   );
 };
 
 export default ForumPost;
-
-
