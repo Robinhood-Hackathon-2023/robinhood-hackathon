72f82e2adaa34368f42671ec20479300bc4d6ce3
Tariqkharsa <Tariq.kharsa@gmail.com>
2023-09-30 15:00:21 -0500

    Improve UI

--- frontend/src/Components/ForumView.jsx
+++ frontend/src/Components/ForumView.jsx
@@ -20,27 +20,79 @@ const ForumView = () => {
   };
 
   return (
-    <div style={{ fontFamily: 'Arial, sans-serif', padding: '20px', backgroundColor: '#f0f0f0', color: '#333', textAlign: 'center' }}>
-      <h1 style={{ color: '#4caf50', marginBottom: '20px' }}>Mental Health Forum</h1>
-      <div style={{ display: 'flex', justifyContent: 'flex-start', marginBottom: '10px', marginLeft: '1150px' }}>
-        <button
-          style={{
-            backgroundColor: '#4caf50',
-            color: 'white',
-            padding: '10px 15px',
-            border: 'none',
-            borderRadius: '5px',
-            cursor: 'pointer',
-          }}
-          onClick={handleSort}
-        >
-          Sorted by Votes: {sortByVotesAsc ? 'Lowest Votes' : 'Highest Votes'}
-        </button>
+    <div
+      style={{
+        fontFamily: "Arial, sans-serif",
+        display: "flex",
+        padding: "20px",
+        backgroundColor: "#f0f0f0",
+        color: "#333",
+      }}
+    >
+      {/* Sidebar */}
+      <div
+        style={{
+          width: "200px",
+          backgroundColor: "#ddd",
+          marginRight: "20px",
+          padding: "10px",
+        }}
+      >
+        {/* Your sidebar content goes here */}
+        Sidebar Content
       </div>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>
-        {posts.map((post) => (
-          <ForumPost key={post.id} post={post} posts={posts} setPosts={setPosts} />
-        ))}
+
+      {/* Main Content */}
+      <div style={{ flex: "1" }}>
+        <h1 style={{ color: "#000", marginBottom: "px" }}>All Posts</h1>
+        <hr style={{ border: "1px solid #ccc", marginBottom: "10px" }} /> 
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginBottom: "10px",
+            alignItems: "center",
+          }}
+        >
+          {/* Move the button to the right */}
+          <div style={{ marginLeft: "auto" }}>
+            <button
+              style={{
+                backgroundColor: "#4caf50",
+                color: "white",
+                padding: "10px 15px",
+                border: "none",
+                borderRadius: "5px",
+                cursor: "pointer",
+              }}
+              onClick={handleSort}
+            >
+              Sorted by Votes:{" "}
+              {sortByVotesAsc ? "Lowest Votes" : "Highest Votes"}
+            </button>
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+          }}
+        >
+          {posts.map((post) => (
+            <div
+              key={post.id}
+              style={{
+                width: "48%",
+                boxSizing: "border-box",
+                padding: "10px",
+                margin: "10px",
+              }}
+            >
+              <ForumPost post={post} posts={posts} setPosts={setPosts} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- frontend/src/Components/mockData.jsx
+++ frontend/src/Components/mockData.jsx
@@ -29,5 +29,35 @@ export const dataPost = [
         { id: 2, user: 'user2', text: 'I learned a lot from this.' },
       ],*/
     },
+    {
+      id: 4,
+      title: "Mental Health Issue",
+      content: "I have been suffering from Depression .......",
+      votes: 0,
+      /*comments: [
+        { id: 1, user: 'user1', text: 'Great post!' },
+        { id: 2, user: 'user2', text: 'I learned a lot from this.' },
+      ],*/
+    },
+    {
+      id: 5,
+      title: "Stress",
+      content: "I am having so much stress at work ...........",
+      votes: 0,
+      /*comments: [
+        { id: 1, user: 'user1', text: 'Great post!' },
+        { id: 2, user: 'user2', text: 'I learned a lot from this.' },
+      ],*/
+    },
+    {
+      id: 6,
+      title: "Financial Difficutly",
+      content: "I need help paying my mortage ...........",
+      votes: 0,
+      /*comments: [
+        { id: 1, user: 'user1', text: 'Great post!' },
+        { id: 2, user: 'user2', text: 'I learned a lot from this.' },
+      ],*/
+    },
     // Add more posts as needed
   ];
